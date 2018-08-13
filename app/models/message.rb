daf1e4000db8c2a10df663014b751f601f80a976
ed1ff <edifei13@yandex.ua>
2018-08-13 16:10:56 +0300

    add cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ApplicationRecord
   validates :text, presence: :true
+
+  after_create :broadcast
+
+  private
+
+  def broadcast
+    ActionCable.server.broadcast('chat', as_json.merge(action: 'CreateMessage'))
+  end
 end
